ef976128bd975fb6dd53fba5bfbd7dbcc569e067
TheViero <128616582+TheViero@users.noreply.github.com>
2023-04-05 16:42:34 +0200

    Prototype

---
+++ Prototype/Prototype/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Prototype;
+using static Prototype.Prototype;
+
+namespace Prototype
+{
+    public partial class Form1 : Form
+    {
+        private Prototype.Pizza _pizza;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Prototype.PizzaBuilder pizzaBuilder = new Prototype.PizzaBuilder(textBox1.Text, textBox2.Text, textBox3.Text, textBox4.Text, textBox5.Text);
+            _pizza = new Prototype.Pizza(pizzaBuilder);
+
+            MessageBox.Show("Успішне створення піци");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_pizza == null)
+            {
+                MessageBox.Show("Будь ласка, створіть піцу перед клонуванням");
+            }
+            else
+            {
+                Prototype.Pizza clonedBread = _pizza.Clone();
+
+                textBox1.Text = clonedBread.Flour;
+                textBox2.Text = clonedBread.Water;
+                textBox3.Text = clonedBread.Yeast;
+                textBox4.Text = clonedBread.Salt;
+                textBox5.Text = clonedBread.Sugar;
+
+                MessageBox.Show("Піца успішно клоновано");
+            }
+
+        }
+    }
+}
---
+++ Prototype/Prototype/Prototype.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototype
+{
+    internal class Prototype
+    {
+        public class PizzaBuilder
+        {
+            public string Flour { get; set; }
+            public string Water { get; set; }
+            public string Yeast { get; set; }
+            public string Salt { get; set; }
+            public string Sugar { get; set; }
+
+            public PizzaBuilder(string flour, string water, string yeast, string salt, string sugar)
+            {
+                Flour = flour;
+                Water = water;
+                Yeast = yeast;
+                Salt = salt;
+                Sugar = sugar;
+            }
+
+            public PizzaBuilder Clone()
+            {
+                return (PizzaBuilder)this.MemberwiseClone();
+            }
+        }
+        public class Pizza
+        {
+            public string Flour { get; set; }
+            public string Water { get; set; }
+            public string Yeast { get; set; }
+            public string Salt { get; set; }
+            public string Sugar { get; set; }
+
+            public Pizza(PizzaBuilder builder)
+            {
+                Flour = builder.Flour;
+                Water = builder.Water;
+                Yeast = builder.Yeast;
+                Salt = builder.Salt;
+                Sugar = builder.Sugar;
+            }
+
+            public Pizza Clone()
+            {
+                return (Pizza)this.MemberwiseClone();
+            }
+        }
+
+    }
+}
